2a03fde9a3c1b62154a87e53bb836a9bce5ca053
Lukhanyo Vakele <lukhanyovakele@gmail.com>
2022-06-03 07:44:05 +0200

    don't search by price regex

--- pizza.js
+++ pizza.js
@@ -8,7 +8,7 @@ const searchBox = document.querySelector('.searchBox');
 // function to filter the data
 const searchFilter = () => {
     const radioBtn = document.querySelector("input[type=radio]:checked")
-    const filteredPizzas = pizzaList.filter(pizza => Object.values(pizza).find(prop => prop.startsWith(searchBox.value || "")))
+    const filteredPizzas = pizzaList.filter(pizza => Object.values(pizza).find(prop => !(/^[0-9?.]+$/.test(prop)) && prop.startsWith(searchBox.value || "")))
     const sortedFilteredPizzas = filteredPizzas.sort((a,b) => {
         return radioBtn.value === "asc" ? (+a.price)-(+b.price) : (+b.price)-(+a.price)
     })
